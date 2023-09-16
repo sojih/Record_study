85c81e299323ff93135457fd42de4a40e0da83c0
Sonh <sojipg0@gmail.com>
2023-09-17 00:07:30 +0900

    09.16 코테

---
+++ src/lv1/Lv1_3.java
@@ -0,0 +1,23 @@
+package lv1;
+
+// 짝수와 홀수
+public class Lv1_3 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
+
+    public static String solution(int num) {
+        // 삼항연산자 사용 - 통과 (0.02ms, 71.1MB)
+//        String answer = (num % 2 == 0) ? "Even" : "Odd";
+//        return answer;
+
+        // if문 사용 - 통과 (0.02ms, 72.3MB)
+        String answer = "";
+        if(num % 2 == 0) {
+            return answer = "Even";
+        } else {
+            return answer = "Odd";
+        }
+
+    }
+}
