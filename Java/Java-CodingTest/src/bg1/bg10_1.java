aa441a9d046df97f52421339e6e32575d8e737f5
Sonh <sojipg0@gmail.com>
2023-09-27 23:25:47 +0900

    09.27 코테 - 백준

---
+++ Java/Java-CodingTest/src/bg1/bg10_1.java
@@ -0,0 +1,21 @@
+package bg1;
+
+import java.io.*;
+
+// 0927 기하 - 1. 직사각형
+public class bg10_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =
+                new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        bw.write(Integer.toString(A * B));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
