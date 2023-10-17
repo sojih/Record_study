a60e8b5036bb0ccf506b30f7afb491359662fb07
Sonh <sojipg0@gmail.com>
2023-10-17 21:32:10 +0900

    10.17 - 코테

---
+++ Java-CodingTest/src/bg1/bg10_8.java
@@ -0,0 +1,13 @@
+package bg1;
+
+import java.util.Scanner;
+
+// 10.17 기하 - 세 막대
+// 주어진 세 변의 길이를 조절(줄이는 것만 가능)해서 나오는 삼각형의 최대 둘레 리턴
+public class bg10_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
